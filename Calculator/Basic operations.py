91f11e5fcb57ff37a5fc69d905112dbe27eb4474
Goabaone <legendaryfan07@gmail.com>
2024-07-31 05:32:52 +0200

    Incomplete calculator

---
+++ Calculator/Basic operations.py
@@ -0,0 +1,22 @@
+def arithmetic_operations():
+    num1 = float(input('Enter first number: '))
+    num2 = float(input('Enter second number: '))
+    operator = input('Enter an operator (+, -, *:, /): ')
+    while operator != '+' or operator != '-' or operator != '*' or operator != '/':
+        print('Invalid entry')
+        operator = input('Enter an operator (+, -, *, /): ')
+    if operator == '+':
+        addition = num1 + num2
+        print(f'{num1} + {num2} = {addition}')
+    elif operator == '-':
+        subtraction = num1 - num2
+        print(f'{num1} - {num2} = {subtraction}')
+    elif operator == '*':
+        multiplication = num1 * num2
+        print(f'{num1} × {num2} = {multiplication}')
+    else:
+        division = num1 / num2
+        print(f'{num1} ÷ {num2} = {division}')
+
+
+arithmetic_operations()
---
+++ Calculator/Root Calculations.py
@@ -0,0 +1,15 @@
+import math
+
+
+def root():
+    num1 = float(input('Enter base number: '))
+    num2 = float(input('Enter root number: '))
+    if num2 == 2:
+        result = math.sqrt(num1)
+        print(f'The square root of {num1} = {result}')
+    elif num2 > 2:
+        result = num1 ** (1 / num2)
+        print(f'The {num2}th root of {num1} = {result}')
+
+
+root()
