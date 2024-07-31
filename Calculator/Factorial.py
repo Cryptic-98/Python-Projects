9aede975865671d44163cd467a291f0af745f474
Goabaone <legendaryfan07@gmail.com>
2024-07-31 10:43:42 +0200

    Incomplete Calculator

---
+++ Calculator/Factorial.py
@@ -0,0 +1,15 @@
+def power():
+    num1 = float(input('Enter base number: '))
+    num2 = float(input('Enter exponent: '))
+    answer = num1 ** num2
+    print(f'{num1} to the power of {num2} = {answer}')
+
+
+def factorial():
+    num = int(input('Enter a number to be factorized: '))
+    fact = 1
+    while num > 2:
+        print('Invalid Entry: factor of 0 does not exist')
+    for x in range(1, num + 1):
+        fact = fact * x
+    print(f'The factorial of {num} = {fact}')
